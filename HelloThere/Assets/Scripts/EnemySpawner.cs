3834ae78620a94d9473beff0248da609298fb29d
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-03-09 16:33:51 +0100

    EnemySpawner.cs: Now functions as intended.
    Co-Authored-By: Benjamin Ting <tingzhb@gmail.com>
    Co-Authored-By: Jasmine Öhlin <90619375+JasmineOhlin@users.noreply.github.com>
    Co-Authored-By: Johannes Danielsson <90214021+MustoLini@users.noreply.github.com>

--- HelloThere/Assets/Scripts/EnemySpawner.cs
+++ HelloThere/Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -10,7 +9,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int maxEnemies;
     public List<GameObject> SpawnableTiles = new List<GameObject>();
 
-    public GameObject EnemyPrefab;
+    [SerializeField] private GameObject EnemyPrefab;
     public void Start()
     {
 
@@ -21,14 +20,18 @@ public class EnemySpawner : MonoBehaviour
     {
         for (int i = 0; i < maxEnemies; i++)
         {
+            if (SpawnableTiles.Count == 0)
+                throw new System.Exception($"No more available spawn tiles! {i} out of {maxEnemies} spawned successfully.");
+            
             var randomIndex = Random.Range(0, SpawnableTiles.Count);
             var spawnableTile = SpawnableTiles[randomIndex];
-            var spawnPostion = spawnableTile.GetComponent<Renderer>().bounds.center;
+            var spawnPosition = spawnableTile.GetComponent<Renderer>().bounds.center;
 
-            spawnPostion.y =+ 1;
-            Instantiate(EnemyPrefab, spawnPostion, Quaternion.identity);
-            Debug.Log("Spawned Eneniems");
+            spawnPosition.y =+ 1;
+            var enemy = Instantiate(EnemyPrefab, spawnPosition, Quaternion.identity);
+            enemy.GetComponent<NetworkObject>().Spawn();
+
+            SpawnableTiles.Remove(spawnableTile);
         }
-
     }
 }
