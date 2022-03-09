4723adcfb15080d3e8515bbf3a98434554afe784
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-03-09 15:58:59 +0100

    EnemySpawner.cs
    Does not instatiet enemy backend problem prob but it is not sure
    
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>
    Co-Authored-By: Jasmine Öhlin <90619375+JasmineOhlin@users.noreply.github.com>
    Co-Authored-By: Benjamin Ting <tingzhb@gmail.com>

--- HelloThere/Assets/Scripts/FloorTile.cs
+++ HelloThere/Assets/Scripts/FloorTile.cs
@@ -1,9 +1,20 @@
+using System;
 using UnityEngine;
 
 
 public class FloorTile : MonoBehaviour
 {
     public bool Spawnable;
+
+    private void Awake()
+    {
+        if (Spawnable)
+        {
+            transform.parent.parent.GetComponent<EnemySpawner>().SpawnableTiles.Add(gameObject);
+
+            // Debug.Log(transform.parent.parent.name);
+        }
+    }
 
     private void OnDrawGizmos()
     {
